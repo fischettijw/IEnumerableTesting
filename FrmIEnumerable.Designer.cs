fec499949288306b66397a1c42cc98d70a00ae1d
Joseph Fischetti <fischettijw@gmail.com>
2020-07-24 10:23:49 -0400

    Add project files.

---
+++ FrmIEnumerable.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace IEnumerableTesting
+{
+    partial class FrmIEnumerable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnTest = new System.Windows.Forms.Button();
+            this.LblRecord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnTest
+            // 
+            this.BtnTest.Location = new System.Drawing.Point(104, 21);
+            this.BtnTest.Name = "BtnTest";
+            this.BtnTest.Size = new System.Drawing.Size(120, 50);
+            this.BtnTest.TabIndex = 0;
+            this.BtnTest.Text = "Test";
+            this.BtnTest.UseVisualStyleBackColor = true;
+            this.BtnTest.Click += new System.EventHandler(this.BtnTest_Click);
+            // 
+            // LblRecord
+            // 
+            this.LblRecord.Location = new System.Drawing.Point(29, 112);
+            this.LblRecord.Name = "LblRecord";
+            this.LblRecord.Size = new System.Drawing.Size(275, 31);
+            this.LblRecord.TabIndex = 1;
+            // 
+            // FrmIEnumerable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 168);
+            this.Controls.Add(this.LblRecord);
+            this.Controls.Add(this.BtnTest);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "FrmIEnumerable";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IEnumerable Testing";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnTest;
+        private System.Windows.Forms.Label LblRecord;
+    }
+}
+
---
+++ FrmIEnumerable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Runtime.CompilerServices;
+using System.IO;
+
+namespace IEnumerableTesting
+{
+    public partial class FrmIEnumerable : Form
+    {
+        public FrmIEnumerable()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTest_Click(object sender, EventArgs e)
+        {
+            IEnumerable<string> lines = File.ReadLines("Data.txt");
+            MessageBox.Show(string.Join(Environment.NewLine, lines));
+            foreach (string line in lines)
+            {
+                //MessageBox.Show(line);
+                if (MessageBox.Show(line, "Database Output", MessageBoxButtons.OKCancel) == DialogResult.Cancel) return;
+            }
+
+        }
+
+    }
+}
+
+
